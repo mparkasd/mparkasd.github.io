486d51aa936d9f6cb5df0a279f17248e44276666
Dimitris Barkas <mparkasd@gmail.com>
2022-10-10 20:14:40 +0300

    headerUpdate

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,14 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Oh, Hi There!</h5>
+        <h1>I'm Dimitris</h1>
+        <h5 className="textLight">Photography enthusiast, F1 fanboy, Web-dev to be.</h5>
+        
+      </div>
+    </header>
   )
 }
 
