60cb63a00bbe8b39b2c0e6c181db675c798c65f0
Dimitris Barkas <mparkasd@gmail.com>
2022-10-11 03:00:56 +0300

    CV Download Update

--- src/components/header/HeaderButtons.jsx
+++ src/components/header/HeaderButtons.jsx
@@ -4,8 +4,8 @@ import CV from '../../assets/cv.pdf'
 const HeaderButtons = () => {
   return (
     <div className='hb'>
-        <a href={CV} download>Download CV</a>
-        <a href='#contact'>Contact Me</a>
+        <a href={CV} download className='btn'>Download CV</a>
+        <a href='#contact' className='btn btnPrimary'>Contact Me</a>
     </div>
   )
 }
