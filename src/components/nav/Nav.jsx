99931477e4def86b87f9427c7aa3356873e4c68a
Dimitris Barkas <mparkasd@gmail.com>
2022-10-13 01:11:10 +0300

    floatingNavBar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './nav.css'
+import { useState } from 'react'
 import {RiHomeLine, RiUserLine, RiCameraLensFill, RiContactsBookLine} from 'react-icons/ri'
 import {AiOutlineMessage} from 'react-icons/ai'
 
 const Nav = () => {
+  const [currentNav, setCurrentNav] = useState ('#')
   return (
     <nav>
-      <a href="#"><RiHomeLine/></a>
-      <a href="#about"><RiUserLine/></a>
-      <a href="#hobbies"><RiCameraLensFill/></a>
-      <a href="#portfolio"><RiContactsBookLine/></a>
-      <a href="#contact"><AiOutlineMessage/></a>
+      <a href="#" onClick={() => setCurrentNav('#')} className={currentNav === '#' ? 'currentNav' : ''}><RiHomeLine/></a>
+      <a href="#about" onClick={() => setCurrentNav('#about')} className={currentNav === '#about' ? 'currentNav' : ''}><RiUserLine/></a>
+      <a href="#hobbies" onClick={() => setCurrentNav('#hobbies')} className={currentNav === '#hobbies' ? 'currentNav' : ''}><RiCameraLensFill/></a>
+      <a href="#portfolio" onClick={() => setCurrentNav('#portfolio')} className={currentNav === '#portfolio' ? 'currentNav' : ''}><RiContactsBookLine/></a>
+      <a href="#contact"onClick={() => setCurrentNav('#contact')} className={currentNav === '#contact' ? 'currentNav' : ''}><AiOutlineMessage/></a>
     </nav>
   )
 }
