3ff36dd80ae720f603e2ae08493ca5d0569da3a7
Dimitris Barkas <mparkasd@gmail.com>
2022-10-15 18:04:22 +0300

    hobbiesSection

--- src/components/hobbies/Hobbies.jsx
+++ src/components/hobbies/Hobbies.jsx
@@ -1,9 +1,77 @@
 import React from 'react'
 import './hobbies.css'
+import {BsCheckCircleFill} from 'react-icons/bs'
 
 const Hobbies = () => {
   return (
-    <section id='hobbies'>Hobbies</section>
+    <section id='hobbies'>
+      <h5>Skills & Hobbies</h5>
+      <h2>OMG</h2>
+
+      <div className="hobbiesContainer">
+        <div className="hobbiesFronEnd">
+          <h3>Web Development</h3>
+          <div className="hobbiesContent">
+            <article className='hobbiesDetails'>
+              <BsCheckCircleFill />
+              <h4>HTML</h4>
+              <small className='textLight'>Intermediate</small>
+            </article>
+            <article className='hobbiesDetails'>
+              <BsCheckCircleFill />
+              <h4>CSS</h4>
+              <small className='textLight'>Intermediate</small>
+            </article>
+            <article className='hobbiesDetails'>
+              <BsCheckCircleFill />
+              <h4>Bootstrap</h4>
+              <small className='textLight'>Intermediate</small>
+            </article>
+            <article className='hobbiesDetails'>
+              <BsCheckCircleFill />
+              <h4>JavaScript</h4>
+              <small className='textLight'>Intermediate</small>
+            </article>
+            <article className='hobbiesDetails'>
+              <BsCheckCircleFill />
+              <h4>React</h4>
+              <small className='textLight'>Intermediate</small>
+            </article>
+          </div>
+        </div>
+
+        <div className="hobbiesBackEnd">
+          <h3>Hobbies</h3>
+            <div className="hobbiesContent">
+              <article className='hobbiesDetails'>
+                <BsCheckCircleFill />
+                <h4>HTML</h4>
+                <small className='textLight'>Intermediate</small>
+              </article>
+              <article className='hobbiesDetails'>
+                <BsCheckCircleFill />
+                <h4>CSS</h4>
+                <small className='textLight'>Intermediate</small>
+              </article>
+              <article className='hobbiesDetails'>
+                <BsCheckCircleFill />
+                <h4>Bootstrap</h4>
+                <small className='textLight'>Intermediate</small>
+              </article>
+              <article className='hobbiesDetails'>
+                <BsCheckCircleFill />
+                <h4>JavaScript</h4>
+                <small className='textLight'>Intermediate</small>
+              </article>
+              <article className='hobbiesDetails'>
+                <BsCheckCircleFill />
+                <h4>React</h4>
+                <small className='textLight'>Intermediate</small>
+              </article>          
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
