2bf52d680c3ddf6fd4cc4159c47f5863b32671c2
Dimitris Barkas <mparkasd@gmail.com>
2022-10-17 19:25:37 +0300

    hobbiesUpdate

--- src/components/hobbies/Hobbies.jsx
+++ src/components/hobbies/Hobbies.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import './hobbies.css'
-import {TbDeviceGamepad2} from 'react-icons/tb'
 import {SiJavascript, SiHtml5, SiReact} from 'react-icons/si'
-import {GiRaceCar, GiPhotoCamera} from 'react-icons/gi'
+import {FaCode} from 'react-icons/fa'
 import {FaCss3} from 'react-icons/fa'
 
 const Hobbies = () => {
@@ -50,21 +49,21 @@ const Hobbies = () => {
           <h3>Hobbies</h3>
             <div className="hobbiesContent">
               <article className='hobbiesDetails'>
-                <GiPhotoCamera className='hobbiesDetailsIcon' />
+                <FaCode className='hobbiesDetailsIcon' />
                 <div>
                   <h4>Photography</h4>
                   <small className='textLight'>I like capturing the urban lifestyle.</small>
                 </div>
               </article>
               <article className='hobbiesDetails'>
-                <GiRaceCar className='hobbiesDetailsIcon' />
+                <FaCode className='hobbiesDetailsIcon' />
                 <div>
                   <h4>Formula 1</h4>
                   <small className='textLight'>Just your everyday F1 enjoyer.</small>
                 </div>
               </article>
               <article className='hobbiesDetails'>
-                <TbDeviceGamepad2 className='hobbiesDetailsIcon' />
+                <FaCode className='hobbiesDetailsIcon' />
                 <div>
                   <h4>Gaming Editor</h4>
                   <small className='textLight'>I love playing games and sometimes write about them.</small>
