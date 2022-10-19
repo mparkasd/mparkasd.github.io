db701df778f1f49422a0fc965e5e2a35463bd42b
Dimitris Barkas <mparkasd@gmail.com>
2022-10-19 21:24:07 +0300

    portfolioUpdate

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -45,20 +45,20 @@ const Portfolio = () => {
             </article>
           </div>
         </div>
-
+        
         <div>
           <h3>Portfolio</h3>
           <div className="portfolioContent">
             <article className='portfolioItem'>
               <h4><SiCheckmarx className='portfolioDetailsIcon' /> My Projects</h4>
-              <div classname='portfolioButtons'>
-                <a href="https://github.com/mparkasd/mparkasd.github.io" className='btn' target='_blank' rel='noreferrer'><FaGithub /> Github</a>
-                <button className='btn btnPrimary' onClick={() => alert("You're already viewing the Demo")}><CgLivePhoto /> Live Demo</button>
-              </div>
+                <small>
+                  <a href="https://github.com/mparkasd/mparkasd.github.io" className='btn' target='_blank' rel='noreferrer'><FaGithub /> Github</a>
+                  <button className='btn btnPrimary' onClick={() => alert("You're already viewing the Demo")}><CgLivePhoto /> Live Demo</button>
+                </small>
             </article>
             <article className='portfolioItem'>
               <h4><SiCheckmarx className='portfolioDetailsIcon' /> My CV</h4>
-              <a href={CV} download className='btn'><FaFileDownload /> Download</a>
+              <small><a href={CV} download className='btn'><FaFileDownload /> Download</a></small>
             </article>
           </div>
         </div>
