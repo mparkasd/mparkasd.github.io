c7145366500ed2f44bf23b3f4ea9b3201c991b7d
Dimitris Barkas <mparkasd@gmail.com>
2022-10-21 21:37:02 +0300

    hobbiesJSX

--- src/components/hobbies/Hobbies.jsx
+++ src/components/hobbies/Hobbies.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
 import './hobbies.css'
+import GAMING from '../../assets/gaming.jpg'
+import PHOTO from '../../assets/gaming.jpg'
+import EDITING from '../../assets/gaming.jpg'
+import MARTIAL from '../../assets/gaming.jpg'
 
 const Hobbies = () => {
   return (
-    <section id='hobbies'>Hobbies</section>
+    <section id='hobbies'>
+      <h5>What I Like</h5>
+      <h2>Hobbies</h2>
+
+      <div className="container hobbiesContainer">
+        <article className="hobbySlide">
+          <div className="hobby">
+            <img src={GAMING} alt="Hobby One" />
+            <h5 className='hobbyName'>Photography</h5>
+            <small className='hobbyDesc'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Quis, ipsa voluptate! Quas sint, architecto ratione quo 
+              atque, animi laborum esse aliquam vero amet quidem. 
+              Vel sed voluptates facere saepe sequi.
+            </small>
+          </div>
+        </article>
+        <article className="hobbySlide">
+          <div className="hobby">
+            <img src={PHOTO} alt="Hobby Two" />
+            <h5 className='hobbyName'>Photography</h5>
+            <small className='hobbyDesc'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Quis, ipsa voluptate! Quas sint, architecto ratione quo 
+              atque, animi laborum esse aliquam vero amet quidem. 
+              Vel sed voluptates facere saepe sequi.
+            </small>
+          </div>
+        </article>
+        <article className="hobby">
+          <div className="hobbyIcon">
+            <img src={EDITING} alt="Hobby Three" />
+            <h5 className='hobbyName'>Photography</h5>
+            <small className='hobbyDesc'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Quis, ipsa voluptate! Quas sint, architecto ratione quo 
+              atque, animi laborum esse aliquam vero amet quidem. 
+              Vel sed voluptates facere saepe sequi.
+            </small>
+          </div>
+        </article>
+        <article className="hobby">
+          <div className="hobbyIcon">
+            <img src={MARTIAL} alt="Hobby Four" />
+            <h5 className='hobbyName'>Photography</h5>
+            <small className='hobbyDesc'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Quis, ipsa voluptate! Quas sint, architecto ratione quo 
+              atque, animi laborum esse aliquam vero amet quidem. 
+              Vel sed voluptates facere saepe sequi.
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
