5ead37dea1171372ea1d1a46c7a53d5726fa05b6
Dimitris Barkas <mparkasd@gmail.com>
2022-10-23 20:14:44 +0300

    hobbiesJSXArray

--- src/components/hobbies/Hobbies.jsx
+++ src/components/hobbies/Hobbies.jsx
@@ -1,6 +1,47 @@
 import React from 'react'
 import './hobbies.css'
 import GAMING from '../../assets/portfolioIMG.jpg'
+import PHOTOGRAPHY from '../../assets/portfolioIMG.jpg'
+import WRITING from '../../assets/portfolioIMG.jpg'
+import FORMULA1 from '../../assets/portfolioIMG.jpg'
+import MARTIALARTS from '../../assets/portfolioIMG.jpg'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    hobby: GAMING,
+    name: 'Gaming',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit Quis, ipsa voluptate! Quas sint, architecto ratione quo atque, animi laborum esse aliquam vero amet quidem.Vel sed voluptates facere saepe sequi'
+  },
+  {
+    hobby: PHOTOGRAPHY,
+    name: 'Photography',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit Quis, ipsa voluptate! Quas sint, architecto ratione quo atque, animi laborum esse aliquam vero amet quidem.Vel sed voluptates facere saepe sequi'
+  },
+  {
+    hobby: WRITING,
+    name: 'Writing',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit Quis, ipsa voluptate! Quas sint, architecto ratione quo atque, animi laborum esse aliquam vero amet quidem.Vel sed voluptates facere saepe sequi'
+  },
+  {
+    hobby: FORMULA1,
+    name: 'Formula 1',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit Quis, ipsa voluptate! Quas sint, architecto ratione quo atque, animi laborum esse aliquam vero amet quidem.Vel sed voluptates facere saepe sequi'
+  },
+  {
+    hobby: MARTIALARTS,
+    name: 'Martial Arts',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit Quis, ipsa voluptate! Quas sint, architecto ratione quo atque, animi laborum esse aliquam vero amet quidem.Vel sed voluptates facere saepe sequi'
+  },
+]
 
 
 const Hobbies = () => {
@@ -9,69 +50,29 @@ const Hobbies = () => {
       <h5>What I Like</h5>
       <h2>Hobbies</h2>
 
-      <div className="container hobbiesContainer">
-        <article className="hobbySlide">
-          <div className="hobby">
-            <img src={GAMING} alt="Gaming" />
-          </div>
-          <h5 className='hobbyName'>Gaming</h5>
-          <small className='hobbyDesc'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Quis, ipsa voluptate! Quas sint, architecto ratione quo 
-            atque, animi laborum esse aliquam vero amet quidem. 
-            Vel sed voluptates facere saepe sequi.
-          </small>
-        </article>
-        <article className="hobbySlide">
-          <div className="hobby">
-            <img src={GAMING} alt="Gaming" />
-          </div>
-          <h5 className='hobbyName'>Photography</h5>
-          <small className='hobbyDesc'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Quis, ipsa voluptate! Quas sint, architecto ratione quo 
-            atque, animi laborum esse aliquam vero amet quidem. 
-            Vel sed voluptates facere saepe sequi.
-          </small>
-        </article>
-        <article className="hobbySlide">
-          <div className="hobby">
-            <img src={GAMING} alt="Gaming" />
-          </div>
-          <h5 className='hobbyName'>Writing</h5>
-          <small className='hobbyDesc'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Quis, ipsa voluptate! Quas sint, architecto ratione quo 
-            atque, animi laborum esse aliquam vero amet quidem. 
-            Vel sed voluptates facere saepe sequi.
-          </small>
-        </article>
-        <article className="hobbySlide">
-          <div className="hobby">
-            <img src={GAMING} alt="Gaming" />
-          </div>
-          <h5 className='hobbyName'>Formula 1</h5>
-          <small className='hobbyDesc'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Quis, ipsa voluptate! Quas sint, architecto ratione quo 
-            atque, animi laborum esse aliquam vero amet quidem. 
-            Vel sed voluptates facere saepe sequi.
-          </small>
-        </article>
-        <article className="hobbySlide">
-          <div className="hobby">
-            <img src={GAMING} alt="Gaming" />
-          </div>
-          <h5 className='hobbyName'>Martial Arts</h5>
-          <small className='hobbyDesc'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Quis, ipsa voluptate! Quas sint, architecto ratione quo 
-            atque, animi laborum esse aliquam vero amet quidem. 
-            Vel sed voluptates facere saepe sequi.
-          </small>
-        </article>
+      <Swiper className="container hobbiesContainer" 
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
 
-      </div>
+        {
+          data.map(({hobby, name, description}, index) => {
+            return(
+              <SwiperSlide key={index} className="hobbySlide">
+                <div className="hobby">
+                  <img src={hobby} alt=""/>
+                </div>
+                <h5 className='hobbyName'>{name}</h5>
+                <small className='hobbyDesc'>
+                  {description}
+                </small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
