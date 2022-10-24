1eba9fd13a1e8f3840ca098f813670a9b65408b9
Dimitris Barkas <mparkasd@gmail.com>
2022-10-24 22:06:48 +0300

    footerJSX

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './footer.css'
+import {BsFacebook, BsInstagram, BsTwitter} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footerLogo'>nutsosya</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="about">About Me</a></li>
+        <li><a href="portfolio">Portfolio</a></li>
+        <li><a href="hobbies">Hobbies</a></li>
+        <li><a href="contact">Contact Me</a></li>
+      </ul>
+
+      <div className="footerSocials">
+        <a href="https://www.instagram.com/nutsosya/" target="_blank" rel='noreferrer'><BsInstagram className='footerIcons'/></a>
+        <a href="https://www.facebook.com/mparkasd" target="_blank" rel='noreferrer'><BsFacebook className='footerIcons'/></a>
+        <a href="https://twitter.com/mparkasd" target="_blank" rel='noreferrer'><BsTwitter className='footerIcons'/></a>
+      </div>
+
+      <div className="footerCopyright">
+        <small>&copy; nutsosya. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
