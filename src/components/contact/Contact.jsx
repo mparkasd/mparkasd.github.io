042cb8e6a54fae162206c200c8a596124657920b
Dimitris Barkas <mparkasd@gmail.com>
2022-10-24 20:34:38 +0300

    contactJSX

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -19,19 +19,24 @@ const Contact = () => {
           </article>
           <article className='contactCard'>
             <MdEmail/>
-            <h4>Email @</h4>
-            <h5>mparkasd@gmail.com</h5>
-            <a href="mailto:mparkasd@gmail.com">Send A Message</a>
+            <h4>Messenger @</h4>
+            <h5>mparkasd</h5>
+            <a href="https://m.me/mparkasd" target='_blank' rel='noreferrer'>Send A Message</a>
           </article>
           <article className='contactCard'>
             <ImWhatsapp/>
             <h4>WhatsApp @</h4>
-            <h5>In case you're a texter.</h5>
+            <h5>+30 693 4054 247</h5>
             <a href="https://api.whatsapp.com/send?phone=306934054247" target='_blank' rel='noreferrer'>Send A Message</a>
           </article>
         </div>
 
-        <form action=""></form>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btnPrimary'>Send Email</button>
+        </form>
       </div>
     </section>
   )
