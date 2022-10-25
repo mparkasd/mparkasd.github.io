f6435cb2943603d025702f5a2dfef7fe0940c53b
Dimitris Barkas <mparkasd@gmail.com>
2022-10-25 21:10:33 +0300

    headerSocials.jsx readability change

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,12 +4,8 @@ import {FiLinkedin, FiGithub} from 'react-icons/fi'
 const HeaderSocials = () => {
   return (
     <div className='headerSocials'>
-        <a href="https://linkedin.com/in/dimitris-barkas" target="_blank" rel='noreferrer'>
-            <FiLinkedin />
-        </a>
-        <a href="https://github.com/mparkasd" target="_blank" rel='noreferrer'>
-            <FiGithub />
-        </a>
+        <a href="https://linkedin.com/in/dimitris-barkas" target="_blank" rel='noreferrer'><FiLinkedin /></a>
+        <a href="https://github.com/mparkasd" target="_blank" rel='noreferrer'><FiGithub /></a>
     </div>
   )
 }
