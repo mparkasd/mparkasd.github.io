5f19cdb021ac4da76a5c861955d4d0c3cac6a5f3
Dimitris Barkas <mparkasd@gmail.com>
2022-10-31 18:39:29 +0200

    about content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -44,7 +44,10 @@ const About = () => {
           </div>
 
           <p>
-            apla xazomares gia to streak.. ntrepomai gia ton eafto mou simera.
+            I'm a young aspiring web-developer with experience on front-end. After reigniting my passion for web developent, I feel ready for the challenge ahead, while I am really intrigued by new technologies and platforms. 
+          </p>
+          <p>
+            I like meeting new people, discusing new and exciting ideas, while also constantly trying to improve myself and help others improve as well. Only together can we go forward!
           </p>
 
           <a href="#contact" className='btn btnPrimary'>Say Hi.</a>
