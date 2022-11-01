f45ab78e0786fd27700f81ead56de46801f2e053
Dimitris Barkas <mparkasd@gmail.com>
2022-11-01 23:27:50 +0200

    hook testing

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './nav.css'
-import { useState } from 'react'
 import {RiHomeLine, RiUserLine, RiCameraLensFill, RiContactsBookLine} from 'react-icons/ri'
 import {AiOutlineMessage} from 'react-icons/ai'
 
 const Nav = () => {
+  const ref = useRef();
+  const onScreen = useOnScreen(ref, "-300px");
   const [currentNav, setCurrentNav] = useState ("#home")
   return (
     <nav>
-      <a href="#home" onClick={() => setCurrentNav("#home")} className={currentNav === "#home" ? 'currentNav' : ''}><RiHomeLine /></a>
-      <a href="#about" onClick={() => setCurrentNav('#about')} className={currentNav === '#about' ? 'currentNav' : ''}><RiUserLine/></a>
-      <a href="#portfolio" onClick={() => setCurrentNav('#portfolio')} className={currentNav === '#portfolio' ? 'currentNav' : ''}><RiContactsBookLine /></a>
-      <a href="#hobbies" onClick={() => setCurrentNav('#hobbies')} className={currentNav === '#hobbies' ? 'currentNav' : ''}><RiCameraLensFill /></a>
-      <a href="#contact" onClick={() => setCurrentNav('#contact')} className={currentNav === '#contact' ? 'currentNav' : ''}><AiOutlineMessage /></a>
+      <a href="#home" ref={ref} onScreen={() => setCurrentNav('#about')} className={currentNav === '#about' ? 'currentNav' : ''} onClick={() => setCurrentNav("#home")} className={currentNav === "#home" ? 'currentNav' : ''}><RiHomeLine /></a>
+      <a href="#about" ref={ref} onScreen={() => setCurrentNav('#about')} className={currentNav === '#about' ? 'currentNav' : ''} onClick={() => setCurrentNav('#about')} className={currentNav === '#about' ? 'currentNav' : ''}><RiUserLine/></a>
+      <a href="#portfolio" onScreen onClick={() => setCurrentNav('#portfolio')} className={currentNav === '#portfolio' ? 'currentNav' : ''}><RiContactsBookLine /></a>
+      <a href="#hobbies" onScreen onClick={() => setCurrentNav('#hobbies')} className={currentNav === '#hobbies' ? 'currentNav' : ''}><RiCameraLensFill /></a>
+      <a href="#contact" onScreen onClick={() => setCurrentNav('#contact')} className={currentNav === '#contact' ? 'currentNav' : ''} ><AiOutlineMessage /></a>
+
+
     </nav>
   )
 }
+
+// Hook
+function useOnScreen(ref, rootMargin = "0px") {
+  // State and setter for storing whether element is visible
+  const [isIntersecting, setIntersecting] = useState(false);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Update our state when observer callback fires
+        setIntersecting(entry.isIntersecting);
+      },
+      {
+        rootMargin,
+      }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      observer.unobserve(ref.current);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+  return isIntersecting;
+}
+
 
 export default Nav
