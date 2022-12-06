2fa5668255a6fa0da707f1d8d5b0680772d78958
Dimitris Barkas <mparkasd@gmail.com>
2022-12-06 17:20:41 +0200

    nav update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 import './nav.css'
-import {RiHomeLine, RiUserLine, RiCameraLensFill, RiContactsBookLine} from 'react-icons/ri'
+import {RiUserLine, RiCameraLensFill, RiContactsBookLine} from 'react-icons/ri'
 import {AiOutlineMessage} from 'react-icons/ai'
 
 const Nav = () => {
@@ -9,13 +9,10 @@ const Nav = () => {
   const [currentNav, setCurrentNav] = useState ("#home")
   return (
     <nav>
-      <a href="#home" ref={ref} onScreen={() => setCurrentNav('#home')} className={currentNav === '#home' ? 'currentNav' : ''} onClick={() => setCurrentNav("#home")} className={currentNav === "#home" ? 'currentNav' : ''}><RiHomeLine /></a>
       <a href="#about" ref={ref} onScreen={() => setCurrentNav('#about')} onClick={() => setCurrentNav('#about')} className={currentNav === '#about' ? 'currentNav' : ''}><RiUserLine/></a>
       <a href="#portfolio" onScreen onClick={() => setCurrentNav('#portfolio')} className={currentNav === '#portfolio' ? 'currentNav' : ''}><RiContactsBookLine /></a>
       <a href="#hobbies" onScreen onClick={() => setCurrentNav('#hobbies')} className={currentNav === '#hobbies' ? 'currentNav' : ''}><RiCameraLensFill /></a>
       <a href="#contact" onScreen onClick={() => setCurrentNav('#contact')} className={currentNav === '#contact' ? 'currentNav' : ''} ><AiOutlineMessage /></a>
-
-
     </nav>
   )
 }
